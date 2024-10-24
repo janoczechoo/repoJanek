d9f9b8502812ff3aa02671b44a34504141229470
Jan Czechowski <jczechowski06@gmail.com>
2024-10-24 13:13:18 +0200

    wyk4 add

---
+++ wyk4_gra.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main ( void )
+{
+    const int MAX_LICZBA = 10;
+    srand ( time( NULL ) );
+    int los = rand() % MAX_LICZBA +1;
+    int odp = 0;
+    int liczbProb = 0;
+    do {
+        printf ( "Pod. liczbe od 1 do %d:(0 konczy gre)",MAX_LICZBA );
+        scanf( "%d", &odp );
+        if( 0 == odp )
+            break;
+        ++liczbProb;
+    } 
+    while ( odp != los );
+    if( odp == los )
+        printf( "Bylo %d prob\n", liczbProb );
+}
